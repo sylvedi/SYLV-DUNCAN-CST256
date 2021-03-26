2c3543ab253d0e451d19327af762aca6009391bb
Cyrus Duncan <cyrusd4264@gmail.com>
2021-03-26 14:19:09 -0700

    Update DebugController.php

--- app/Http/Controllers/DebugController.php
+++ app/Http/Controllers/DebugController.php
@@ -6,6 +6,18 @@ use Illuminate\Http\Request;
 
 class DebugController extends Controller
 {
+        private $logger;
+    
+    /**
+     * Instantiates the object with a database connection
+     *
+     * @param ILoggerService $logger
+     */
+    public function __construct(ILoggerService $logger)
+    {
+        $this->logger = $logger;
+    }
+    
     
     public function scratchPad(Request $request){
         
